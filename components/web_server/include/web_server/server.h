0ea2146d606f7cd7bd9b2b00678df6d5490c039b
FiendChain <formulating.response@gmail.com>
2020-02-14 13:25:12 +1100

    Updated with autogenerated webserver

--- components/web_server/include/web_server/server.h
+++ components/web_server/include/web_server/server.h
@@ -1,8 +1,10 @@
-#ifndef __WEBSERVER_INIT_H__
-#define __WEBSERVER_INIT_H__
+
+#ifndef __WEBSERVER_H__ 
+#define __WEBSERVER_H__
 
 #include <esp_http_server.h>
 
 httpd_handle_t start_webserver(uint32_t port);
 
 #endif
+    
